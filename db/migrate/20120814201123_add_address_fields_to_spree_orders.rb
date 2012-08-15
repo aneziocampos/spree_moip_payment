4f1a56bd7d9d37bdb30ecaefca5419328cb2c4f4
Mauro George <maurogot@gmail.com>
2012-08-15 10:09:32 -0300

    remove unused migration

--- db/migrate/20120814201123_add_address_fields_to_spree_orders.rb
+++
@@ -1,6 +0,0 @@
-class AddAddressFieldsToSpreeOrders < ActiveRecord::Migration
-  def change
-    add_column :spree_orders, :address_number, :string
-    add_column :spree_orders, :district, :string
-  end
-end
