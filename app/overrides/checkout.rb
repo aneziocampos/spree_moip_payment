5b86070e1908f5eda3f994c6076ea66061f803e1
Mauro George <maurogot@gmail.com>
2012-08-15 11:06:24 -0300

    added shipp address fields

--- app/overrides/checkout.rb
+++ app/overrides/checkout.rb
@@ -2,3 +2,8 @@ Deface::Override.new(:virtual_path => "spree/checkout/_address",
                      :name => "checkout_bill_address",
                      :insert_before => "#bzipcode",
                      :partial => "spree/checkout/bill_address_fields")
+
+Deface::Override.new(:virtual_path => "spree/checkout/_address",
+                    :name => "checkout_ship_address",
+                    :insert_before => "#szipcode",
+                    :partial => "spree/checkout/ship_address_fields")
