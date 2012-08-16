c215a54fcb05af08b7ef152afffaef492f44c14a
Rodrigo Pinto <rodrigopqn@gmail.com>
2012-08-16 20:24:23 -0300

    Update factory names to make sense

--- spec/factories/moip_payment.rb
+++ spec/factories/moip_payment.rb
@@ -1,3 +1,4 @@
 Factory.define :moip_payment, :parent => :payment do |record|
   record.association(:order, factory: :moip_order)
+  record.association(:payment_method, factory: :moip_payment_method)
 end
--- spec/factories/moip_payment_method.rb
+++ spec/factories/moip_payment_method.rb
@@ -1,3 +1,4 @@
-Factory.define :moip, :class => Spree::PaymentMethod::Moip, :parent => :payment_method do |record|
+Factory.define :moip_payment_method, :class => Spree::PaymentMethod::Moip, :parent => :payment_method do |record|
   record.name 'Moip'
+  record.environment 'test'
 end
