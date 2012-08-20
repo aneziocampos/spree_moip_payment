2f9fa1f2c2ef4775c41dc023317e68c0f03bd9d2
Mauro George <maurogot@gmail.com>
2012-08-20 15:54:48 -0300

    on boleto shoing name and last name

--- lib/moipr/enviar_instrucao_xml.rb
+++ lib/moipr/enviar_instrucao_xml.rb
@@ -12,7 +12,7 @@ module Moipr
           instrucao_unica.IdProprio @order.number # V64
           instrucao_unica.Valores { |v| v.Valor(@order.total, 'moeda' => 'BRL') }# decimal(9,2)
           instrucao_unica.Pagador do |p|
-            p.Nome @order.bill_address.firstname # 256
+            p.Nome "#{@order.bill_address.firstname} #{@order.bill_address.lastname}" # 256
             p.Email @order.user.email # 45
             p.IdPagador @order.user.id # 20
             p.EnderecoCobranca do |e|
